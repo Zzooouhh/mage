400e6a38eee7b476b430cca1025e5b7599373dd8
L_J <Zzooouhh@users.noreply.github.com>
2018-02-12 22:31:43 +0000

    Altered canBlock to support blocking bands

--- Mage/src/main/java/mage/game/permanent/PermanentImpl.java
+++ Mage/src/main/java/mage/game/permanent/PermanentImpl.java
@@ -1121,32 +1121,46 @@ public abstract class PermanentImpl extends CardImpl implements Permanent {
         if (tapped && !game.getState().getContinuousEffects().asThough(this.getId(), AsThoughEffectType.BLOCK_TAPPED, this.getControllerId(), game)) {
             return false;
         }
-        Permanent attacker = game.getPermanent(attackerId);
-        if (attacker == null) {
+        Permanent baseAttacker = game.getPermanent(attackerId);
+        if (baseAttacker == null) {
             return false;
         }
-        // controller of attacking permanent must be an opponent
-        if (!game.getPlayer(this.getControllerId()).hasOpponent(attacker.getControllerId(), game)) {
-            return false;
-        }
-        //20101001 - 509.1b
-        // check blocker restrictions
-        for (Map.Entry<RestrictionEffect, Set<Ability>> entry : game.getContinuousEffects().getApplicableRestrictionEffects(this, game).entrySet()) {
-            for (Ability ability : entry.getValue()) {
-                if (!entry.getKey().canBlock(attacker, this, ability, game)) {
-                    return false;
+        game.informPlayers(baseAttacker.getBandedCards().size() + " banded cards for " + baseAttacker.getLogName());
+        List<UUID> attackerIdsToCheck = new ArrayList<>(baseAttacker.getBandedCards()); // handles banding
+        attackerIdsToCheck.add(attackerId);
+        game.informPlayers(attackerIdsToCheck.size() + " bandedIds");
+        blockCheck:
+        for (UUID bandedId : attackerIdsToCheck) {
+            Permanent attacker = game.getPermanent(bandedId);
+            if (attacker == null) {
+                continue blockCheck;
+            }
+            // controller of attacking permanent must be an opponent
+            if (!game.getPlayer(this.getControllerId()).hasOpponent(attacker.getControllerId(), game)) {
+                continue blockCheck;
+            }
+            //20101001 - 509.1b
+            // check blocker restrictions
+            for (Map.Entry<RestrictionEffect, Set<Ability>> entry : game.getContinuousEffects().getApplicableRestrictionEffects(this, game).entrySet()) {
+                for (Ability ability : entry.getValue()) {
+                    if (!entry.getKey().canBlock(attacker, this, ability, game)) {
+                        continue blockCheck;
+                    }
                 }
             }
-        }
-        // check also attacker's restriction effects
-        for (Map.Entry<RestrictionEffect, Set<Ability>> restrictionEntry : game.getContinuousEffects().getApplicableRestrictionEffects(attacker, game).entrySet()) {
-            for (Ability ability : restrictionEntry.getValue()) {
-                if (!restrictionEntry.getKey().canBeBlocked(attacker, this, ability, game)) {
-                    return false;
+            // check also attacker's restriction effects
+            for (Map.Entry<RestrictionEffect, Set<Ability>> restrictionEntry : game.getContinuousEffects().getApplicableRestrictionEffects(attacker, game).entrySet()) {
+                for (Ability ability : restrictionEntry.getValue()) {
+                    if (!restrictionEntry.getKey().canBeBlocked(attacker, this, ability, game)) {
+                        continue blockCheck;
+                    }
                 }
             }
+            if (!attacker.hasProtectionFrom(this, game)) {
+                return true;
+            }
         }
-        return !attacker.hasProtectionFrom(this, game);
+        return false;
     }
 
     @Override
